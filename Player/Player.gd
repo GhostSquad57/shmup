469c5e04c0b157cf81bdd48f6912520376ae0f73
Keith Henderson Jr <keithhendersonjr@gmail.com>
2020-05-01 18:59:51 -0400

    Added boundries

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,3 +15,13 @@ func _process(delta):
 	velocity.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
 	position = position + velocity * speed * delta
+
+	if position.y < 0:
+		position.y = 0
+	elif position.y > get_viewport().get_visible_rect().size.y:
+		position.y = get_viewport().get_visible_rect().size.y
+
+	if position.x < 0:
+		position.x = 0
+	elif position.x > get_viewport().get_visible_rect().size.x:
+		position.x = get_viewport().get_visible_rect().size.x
