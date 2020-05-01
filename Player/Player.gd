cf874bbfeab4e598a93fa1187057ce35d0e26ab6
Sockstah <yansan95@gmail.com>
2020-05-02 00:12:39 +0200

    initial godot project setup basic player movement

---
+++ Player/Player.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+export (int) var speed = 100
+var velocity = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+
+	position = position + velocity * speed * delta
